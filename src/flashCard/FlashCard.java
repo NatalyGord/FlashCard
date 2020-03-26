3018ddd615c286db8fcdaa6561c32e0b1f053e2d
Nataliya Gordiets <NatGord@rambler.ru>
2020-03-26 20:28:04 +0600

    часть 3 из 7

--- src/flashCard/FlashCard.java
+++ src/flashCard/FlashCard.java
@@ -3,17 +3,29 @@ import java.util.Scanner;
 public class FlashCard {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String card = scanner.nextLine();
-        String definition = scanner.nextLine();
-        System.out.println(card);
-        System.out.println(definition);
-        String ans = scanner.nextLine();
-        System.out.println(ans);
-        if(ans.equals(definition)){
-            System.out.println("Your answer is right!");
+        System.out.println("Input the number of cards:");
+        int n = scanner.nextInt();
+        String a = scanner.nextLine();
+        String[] arrCard = new String[n];
+        String[] arrDef = new String[n];
+        for(int i = 1; i <= n; i++){
+            System.out.printf("The card #%d: %n", i);
+            arrCard[i - 1] = scanner.nextLine();
+            System.out.println(arrCard[i - 1]);
+            System.out.printf("The definition of the card #%d: %n", i);
+            arrDef[i - 1] = scanner.nextLine();
+            System.out.println(arrDef[i - 1]);
         }
-        else{
-            System.out.println("Your answer is wrong...");
+        for(int i = 0; i < n; i++) {
+            System.out.printf("Print the definition of \"%s\": %n", arrCard[i]);
+
+            String ans = scanner.nextLine();
+            System.out.println(ans);
+            if (ans.equals(arrDef[i])) {
+                System.out.println("Correct answer.");
+            } else {
+                System.out.printf("Wrong answer. The correct one is \"%s\". %n", arrDef[i]);
+            }
         }
     }
 }
