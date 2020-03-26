e6763a9eecc12e9e6315d31a72ad7299c958c94f
Nataliya Gordiets <NatGord@rambler.ru>
2020-03-26 13:21:28 +0600

    часть 1 из 7

---
+++ src/flashCard/FlashCard.java
@@ -0,0 +1,10 @@
+package flashCard;
+
+public class FlashCard {
+    public static void main(String[] args) {
+        System.out.println("Card: ");
+        System.out.println("cat");
+        System.out.println("Definition: ");
+        System.out.println("кот");
+    }
+}
